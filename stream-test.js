c24ee6ccb1ec80c5efab124b7614394e1f0780d8
Justin Merz <jrmerz@gmail.com>
2019-10-08 17:24:17 -0700

    bug fixes, start of csv debugging

--- stream-test.js
+++ stream-test.js
@@ -2,4 +2,5 @@ const processor = require('./lib/binary-stream-processor');
 // processor();
 
 const fs = require('fs');
-processor(fs.createReadStream('subgrabpackets.dat'));
+// processor(fs.createReadStream('subgrabpackets.dat'));
+processor(fs.createReadStream('7am_g.dat'));
