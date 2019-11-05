018e6277f1252df42e36191dd75c6d6965641744
Justin Merz <jrmerz@gmail.com>
2019-11-05 12:11:30 -0800

    adding HTTP POST streaming options

--- stream-test.js
+++ stream-test.js
@@ -1,6 +1,25 @@
-const processor = require('./lib/binary-stream-processor');
+const Processor = require('./lib/binary-stream-processor');
 // processor();
 
 const fs = require('fs');
 // processor(fs.createReadStream('subgrabpackets.dat'));
-processor(fs.createReadStream('8am_g.dat'));
+
+let processor = new Processor({
+  imageBlock : {
+    post : {
+      url : 'http://localhost:3000',
+      headers : {
+        authorization : 'bearer f21das98asdjkl39'
+      }
+    }
+  },
+  generic : {
+    post : {
+      url : 'http://localhost:3000',
+      headers : {
+        authorization : 'bearer f21das98asdjkl39'
+      }
+    }
+  }
+})
+processor.pipe(fs.createReadStream('8am_g.dat'));
