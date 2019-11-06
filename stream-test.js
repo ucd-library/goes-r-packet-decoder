d7d182df3b685682006e01bf6f57bb7c49de9936
Justin Merz <jrmerz@gmail.com>
2019-11-05 16:38:05 -0800

    fix bug fixes

--- stream-test.js
+++ stream-test.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 // processor(fs.createReadStream('subgrabpackets.dat'));
 
 let processor = new Processor({
+  filter : /^91$/i,
   imageBlock : {
     post : {
       url : 'http://localhost:3000',
